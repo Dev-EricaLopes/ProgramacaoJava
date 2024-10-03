e27569d7fd5fe3d5c08e7e739df6f0bf40358b62
Erica Lopes <101986065+Dev-EricaLopes@users.noreply.github.com>
2024-10-03 11:24:36 -0300

    aula 03/10

---
+++ ProjetoA3/src/main/java/com/mycompany/projetoa3/ProjetoA3.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.projetoa3;
+
+/**
+ *
+ * @author erica
+ */
+public class ProjetoA3 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
